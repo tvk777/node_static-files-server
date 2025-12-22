67600f19b9021ef4b67d80cf0c959c21d754b285
tvk777 <klimenko.tv7@gmail.com>
2025-12-22 16:24:32 +0200

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,44 @@
+/* eslint-disable no-console */
+
 'use strict';
 
+const http = require('http');
+const fsp = require('fs/promises');
+const path = require('path');
+const { validateRequest } = require('./validateRequest');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer(async (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+
+    const filePath = req.url.slice(1);
+    const { code, message } = validateRequest(filePath);
+
+    if (code) {
+      res.statusCode = code;
+      res.end(message);
+
+      return;
+    }
+
+    try {
+      const safePath = path.join(
+        'public',
+        filePath.replace(/^file\/?/, '') || 'index.html',
+      );
+
+      const file = await fsp.readFile(safePath, 'utf-8');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(file);
+    } catch (error) {
+      res.statusCode = 404;
+      res.end(`File ${filePath} is Not Found'`);
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
---
+++ src/validateRequest.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+const validateRequest = (filePath) => {
+  const result = {
+    code: null,
+    message: null,
+  };
+
+  if (filePath.includes('..') || !filePath.includes('file')) {
+    result.code = 400;
+    result.message = 'Bad request';
+
+    return result;
+  }
+
+  if (!filePath.startsWith('file/')) {
+    result.code = 200;
+    result.message = 'Request should start with "file/"';
+
+    return result;
+  }
+
+  if (filePath.includes('//')) {
+    result.code = 404;
+    result.message = 'Request should not contain double slashes';
+
+    return result;
+  }
+
+  return result;
+};
+
+module.exports = { validateRequest };
